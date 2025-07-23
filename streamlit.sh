ab76b6a0be451d3c63ff73bf8a5db03b0184c78e
kjha0428 <kjha7448@gmail.com>
2025-07-23 20:31:53 +0900

    test

--- streamlit.sh
+++ streamlit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-pip install --upgrade pip
-pip uninstall -y orjson streamlit
-pip install --no-cache-dir orjson==3.9.10
-pip install --no-cache-dir streamlit==1.31.0
-pip install -r requirements.txt
-python -m streamlit run main.py --server.port 8000 --server.address 0.0.0.0
+# pip install --upgrade pip
+# pip uninstall -y orjson streamlit
+# pip install --no-cache-dir orjson==3.9.10
+# pip install --no-cache-dir streamlit==1.31.0
+# pip install -r requirements.txt
+# python -m streamlit run main.py --server.port 8000 --server.address 0.0.0.0
